eb90ed2c345f0ee1ea99f516fa922768325eb813
brainsrage@gmail.com <2548561qwe>
2015-06-25 16:52:47 +0500

    init

---
+++ Instagram.php
@@ -0,0 +1,61 @@
+<?php
+namespace brainsrage\authclient;
+
+use yii\authclient\OAuth2;
+
+/**
+ * Created by PhpStorm.
+ * User: brain_000
+ * Date: 25.06.2015
+ * Time: 15:47
+ */
+class Instagram extends OAuth2
+{
+    /**
+     * @inheritdoc
+     */
+    public $authUrl = 'https://api.instagram.com/oauth/authorize';
+    /**
+     * @inheritdoc
+     */
+    public $tokenUrl = 'https://api.instagram.com/oauth/access_token';
+    /**
+     * @inheritdoc
+     */
+    public $apiBaseUrl = 'https://api.instagram.com/v1';
+
+    /**
+     * @inheritdoc
+     */
+    protected function initUserAttributes()
+    {
+        $response = $this->api('users/self', 'GET');
+
+        return $response['data'];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function apiInternal($accessToken, $url, $method, array $params, array $headers)
+    {
+        return $this->sendRequest($method, $url . '?access_token=' . $accessToken->getToken(), $params, $headers);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultName()
+    {
+        return 'instagram';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function defaultTitle()
+    {
+        return 'Instagram';
+    }
+}
+
